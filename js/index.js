66d5bfdab4b4c0f4439097fe702add0712ea0ae4
Muhammad Akbar Endy Wijaya <42760151+akbar-pekat@users.noreply.github.com>
2020-12-11 21:10:47 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -13,14 +13,24 @@ var categorysection;
 $("#itemsmore").click(function() {
   categorysection = 1;
   $(".category-section").css("bottom", "0px");
+  $("body").css("overflow", "hidden");
   $(".shadow").fadeIn();
+  console.log("");
 });
 $(".categorys .group .items").click(function() {
   categorysection = 0;
+  $("body").css("overflow", "scroll");
   $(".category-section").css("bottom", "-30%");
   $("#title-container").text(this.innerText);
   $('.category .group .items').removeClass("active");
   $(".shadow").fadeOut();
+  $(".container").hide();
+  $(".loader-section").fadeIn();
+
+  setTimeout(function() {
+    $(".loader-section").hide();
+    $(".container").fadeIn();
+  }, 1500);
 });
 
 document.addEventListener('touchstart', handleTouchStart, false);
@@ -56,6 +66,8 @@ function handleTouchMove(evt) {
       if (categorysection == 1) {
         categorysection = 0;
         $(".category-section").css("bottom", "-30%");
+        $("body").css("overflow", "scroll");
+
         $(".shadow").fadeOut();
       }
     }
@@ -68,11 +80,24 @@ $('.category .group .items').click(function() {
   $('.category .group .items').removeClass("active");
   $("#title-container").text(this.innerText);
   $(this).addClass("active");
+  $(".container").hide();
+  $(".loader-section").fadeIn();
+
+  setTimeout(function() {
+    $(".loader-section").hide();
+    $(".container").fadeIn();
+  }, 1500);
 });
 
 $(document).ready(function() {
   $(".category-section .content .categorys .group .items").css("animation", "none");
   $(".category-section .content .categorys .group .items").css("background", "transparent");
+  $(".loader-section").fadeIn();
+
+  setTimeout(function() {
+    $(".container").fadeIn();
+    $(".loader-section").hide();
+  }, 1500);
 });
 
 window.onscroll = function() {
