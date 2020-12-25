2890b7ebf2eb7ec481dddaa492a8a38e3b574bc8
Muhammad Akbar Endy Wijaya <42760151+akbar-pekat@users.noreply.github.com>
2020-12-25 10:24:58 +0800

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -1,23 +1,65 @@
-const staticMangan = "Mangan-site-v1"
-const assets = [
-  "/",
-  "index.html",
-  "css/index.css",
-  "js/index.js"
-]
+var CACHE_NAME = 'mangan-cache-v1';
+var urlsToCache = [
+  '/',
+  'index.html'
+];
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticMangan).then(cache => {
-      cache.addAll(assets)
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
     })
-  )
-})
+  );
+});
 
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).then(
+        function(response) {
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          // IMPORTANT: Clone the response. A response is a stream
+          // and because we want the browser to consume the response
+          // as well as the cache consuming the response, we need
+          // to clone it so we have two streams.
+          var responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+          .then(function(cache) {
+            cache.put(event.request, responseToCache);
+          });
+          return response;
+        }
+      );
     })
-  )
-})
+  );
+});
+
+self.addEventListener('activate', function(event) {
+
+  var cacheAllowlist = CACHE_NAME;
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
